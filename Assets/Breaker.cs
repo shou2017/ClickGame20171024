167298c20bdac7bfb2c390a10c22f894ceb1c1ed
Wataru Ozeki <nkc.wataru.ozeki@gmail.com>
2017-10-26 10:44:06 +0900

    no message

---
+++ Assets/Breaker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breaker : MonoBehaviour {
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //shoot(new Vector3(0, 0, 2000));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BreakerGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakerGenerator : MonoBehaviour {
+
+    public GameObject BreakerPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Breaker = Instantiate(BreakerPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            Breaker.GetComponent<Breaker>().shoot(worldDir.normalized * 2000);
+        }
+    }
+}
